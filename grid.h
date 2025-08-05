85a6300ddc5bd55e238afe706aede962bf66ee17
tinytinus <tinyducky1@proton.me>
2025-08-05 07:37:49 +0200

    grid managment

--- grid.h
+++ grid.h
@@ -8,10 +8,34 @@ typedef struct {
 } Grid2D;
 
 // Grid management
-Grid2D* grid_create(int width, int height);
-void grid_destroy(Grid2D *grid);
-void grid_clear(Grid2D *grid);
-void grid_copy(Grid2D *dest, Grid2D *src);
+Grid2D* grid_create(int width, int height) {
+	Grid2D* grid = malloc(sizeof(Grid2D));
+	grid->data = malloc(width * height * sizeof(float));
+	grid->width = width;
+	grid->height = height;
+
+	memset(grid->data, 0, width * height * sizeof(float));
+	return grid;	
+}
+
+void grid_destroy(Grid2D *grid) {
+	if (grid) {
+		free(grid->data);
+		free(grid);
+	}
+}
+
+void grid_clear(Grid2D *grid) {
+	if (grid) {
+		memset(grid->data, 0, width * height * sizeof(float));
+	}
+}
+
+void grid_copy(Grid2D *dest, Grid2D *src) {
+	if (dest->height == src->height && dest->width == src->width) {
+		memcpy(dest->data, src->data, src->width * src->height * sizeof(float));
+	}
+}
 
 // Grid access (with bounds checking)
 float grid_get(Grid2D *grid, int x, int y);
