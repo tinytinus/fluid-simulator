df95541e9a6a70be390d21093793ec7e233fd7e3
tinytinus <tinyducky1@proton.me>
2025-08-27 19:30:05 +0200

    added Grid.set

--- grid.cs
+++ grid.cs
@@ -51,15 +51,41 @@ public class Grid {
 	 *
 	 * works by:
 	 * 	checking if the grid exists
-	 * 	getting the value from that position directly
+	 * 	clamp the position to stay withing the grid
+	 * 	getting the value from that position 
 	 * 	returning that value
 	 */
 	public static float get(Grid2D grid, int x, int y) {
 		if (!grid) return 0.0f; 
 
-		// TODO: this should be clamped
+		if (x > grid.width) x = grid.width;
+		if (x < 0) x = 0;
+		if (y > grid.height) y = grid.height;
+		if (y < 0) y = 0;
 
 		float value = grid.data[x, y];
 		return value;
+	}
+
+	/*
+	 * Grid.set
+	 * sets a position on a grid to a value
+	 * 	@param grid - the grid to target
+	 * 	@param x - the x position
+	 * 	@param y - the y position
+	 * 	@param value - the value to set the position to 
+	 *
+	 * works by:
+	 * 	checking if the grid exists
+	 * 	checking if the position is inside of the grid
+	 *	setting the position to the value
+	 */
+	public static void set(Grid2D grid, int x, int y, float value) {
+		if (!grid) return;
+
+		if (x > grid.width || x < 0) return;
+		if (y > grid.height || y < 0) return;
+
+		grid.data[x, y] = value;
 	}
 }
